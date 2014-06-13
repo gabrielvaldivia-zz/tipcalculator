b63403a1f60b0107a2830537846e17b208e6ead2
Gabriel Valdivia <valdivia.gabriel@gmail.com>
2014-06-13 12:10:56 -0700

    Initial commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Gabriel Valdivia on 6/12/14.
+//  Copyright (c) 2014 Gabriel Valdivia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
